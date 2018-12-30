22d7b96e285e55c17d1eadbf37d15fc535f2eb24
Joe Ganobsik <jganobsik@gmail.com>
2018-12-30 13:18:06 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,7 +1,10 @@
 class Triangle
   # write code here
   def initialize (l1,l2, l3)
-    if l1
+    if l1 == l2 && l2 == l3 
+      return 'equilateral'
+      elsif 
+      
   end
     class TriangleError < StandardError
     # triangle error code
