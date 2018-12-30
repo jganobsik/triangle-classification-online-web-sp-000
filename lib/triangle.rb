8f82797afebce7d2ce9b744c62c8b42c4ca3bbf5
Joe Ganobsik <jganobsik@gmail.com>
2018-12-30 13:23:02 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,7 +2,7 @@ class Triangle
   # write code here
   def initialize (l1,l2, l3)
     
-    
+    def kind(l1,l2,l3)
     if l1 == l2 && l2 == l3 
       return 'equilateral'
       elsif l1 == l2 || l2 == l3 || l1 == l3
