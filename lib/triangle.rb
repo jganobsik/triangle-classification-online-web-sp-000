2d90e21ad38fd331b5049e297b5ed9912a1ef19a
Joe Ganobsik <jganobsik@gmail.com>
2018-12-30 13:35:45 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,8 +12,7 @@ class Triangle
      if @side1 !> 0 || @side2 !> 0 || @side3 !> 0 
        begin
         raise TriangleError
-      rescue TriangleError => error
-          puts error.message
+      rescue TriangleError
       end
       elsif 
     end
