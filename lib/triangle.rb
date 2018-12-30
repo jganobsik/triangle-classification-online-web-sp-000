2b5182f22f86f06717dd07134e5b3c2607320958
Joe Ganobsik <jganobsik@gmail.com>
2018-12-30 13:37:51 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
     @side3 = l3
     
     
-     if @side1 > 0 || @side2 !> 0 || @side3 !> 0 
+     if @side1 >= 0 == false || @side2 >= 0 == false || @side3 >= 0 == false
        begin
         raise TriangleError
         rescue TriangleError
